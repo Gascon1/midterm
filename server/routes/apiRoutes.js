f875b1166fe1785154f8ff55389358281447b176
Mikel Gascon <mikelgascon@outlook.com>
2019-11-16 20:50:23 +0000

    updated search method to search the whole string and not only the datatype

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -1,7 +1,7 @@
 const request = require("request");
 const userInput = "harry potter";
 const encodedUserInput = encodeURI(userInput);
-const AppId = 'YJ5XA9-EUGVJUHHHH'
+const AppId = "YJ5XA9-EUGVJUHHHH";
 
 const URL = `http://api.wolframalpha.com/v2/query?input=${encodedUserInput}&output=json&appid=${AppId}`;
 // const URL = 'http://api.wolframalpha.com/v2/query?appid=YJ5XA9-EUGVJUHHHH&input=harry%20potter&output=json'
@@ -10,17 +10,23 @@ request(URL, function(error, response, body) {
   // console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
   // console.log('typeof body:', body.search('Products')); // Print the HTML for the Google homepage.
   let parsedBody = JSON.parse(body);
-
-
+  let datatype = parsedBody.queryresult.datatypes;
 
   // checking if the query succeeds
+  // if (parsedBody.queryresult.success) {
+  //   console.log('datatype : ', datatype);
+  //   console.log('tyopeof datatype : ', typeof datatype)
+  //   console.log(datatype.search("Fictional"))
+  //   // console.log('body ; ', body)
+  //   if (datatype.search("Fictional") !== - 1) {
+  //     console.log('This is a fictional character')
+  //   }
+
+  // }
+
   if (parsedBody.queryresult.success) {
-    console.log('datatype : ', parsedBody.queryresult.datatypes);
-    console.log('body ; ', body)
-
-
-
-
+    if (body.toLowerCase().search("movie")) {
+      console.log("this is a movie");
+    }
   }
-
 });
