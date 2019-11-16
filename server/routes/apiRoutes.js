ff44728cb17bb54900ba9638633b6c0c7cdcdc1d
Mikel Gascon <mikelgascon@outlook.com>
2019-11-16 20:29:11 +0000

    changed the components of the query string

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -1,21 +1,15 @@
-const request = require('request');
-const userInput = 'harry potter'
-const encodedUserInput = encodeURI(userInput)
+const request = require("request");
+const userInput = "harry potter";
+const encodedUserInput = encodeURI(userInput);
+const AppId = 'YJ5XA9-EUGVJUHHHH'
 
-
-const URL = `http://api.wolframalpha.com/v2/query?appid=YJ5XA9-EUGVJUHHHH&input=${encodedUserInput}&output=json`
+const URL = `http://api.wolframalpha.com/v2/query?input=${encodedUserInput}&output=json&appid=${AppId}`;
 // const URL = 'http://api.wolframalpha.com/v2/query?appid=YJ5XA9-EUGVJUHHHH&input=harry%20potter&output=json'
-request(URL, function (error, response, body) {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body.pods:', body); // Print the HTML for the Google homepage.
+request(URL, function(error, response, body) {
+  // console.log("error:", error); // Print the error if one occurred
+  // console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
   // console.log('typeof body:', body.search('Products')); // Print the HTML for the Google homepage.
-// let parsedBody = JSON.parse(body);
-// console.log(parsedBody);
-//
-  // console.log(parsedBody.queryresult.datatypes);
-  // console.log(body.search('Products'))
-//
-
-console.log(URL);
+  let parsedBody = JSON.parse(body);
+  console.log(parsedBody.queryresult.datatypes);
+  console.log(URL)
 });
