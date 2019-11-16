53f0e59beb13065faa2a459b62363cca98c76f23
SergeRodriguez <serge.rodriguez@mail.mcgill.ca>
2019-11-16 18:37:11 +0000

    testing wolfram alpha

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -4,10 +4,10 @@ const URL = 'http://api.wolframalpha.com/v2/query?appid=YJ5XA9-EUGVJUHHHH&input=
 request(URL, function (error, response, body) {
   console.log('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  // console.log('typeof body:', body.search('Products')); // Print the HTML for the Google homepage.
-  // console.log('body.pods:', body); // Print the HTML for the Google homepage.
-let parsedBody = JSON.parse(body);
-console.log(parsedBody);
+  console.log('typeof body:', body.search('Products')); // Print the HTML for the Google homepage.
+  console.log('body.pods:', body); // Print the HTML for the Google homepage.
+// let parsedBody = JSON.parse(body);
+// console.log(parsedBody);
 
-  console.log(parsedBody.queryresult.datatypes);
+  // console.log(parsedBody.queryresult.datatypes);
 });
