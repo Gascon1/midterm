b8d64acf61b2a1f7ace7f215edcf284747ad518c
SergeRodriguez <serge.rodriguez@mail.mcgill.ca>
2019-11-18 18:14:02 +0000

    add fake todos data in database

---
+++ db/seeds/03_todo_items.sql
@@ -0,0 +1,12 @@
+-- todo_items table seeds here (Example)
+INSERT INTO todo_items (name, is_completed, category_id, user_id)
+VALUES
+('Joker', false, 1, 1),
+('Arrival', false, 1, 1),
+('Bohemian Rapsody', false, 1, 1),
+('The Avatar', false, 1, 1),
+('Batman', false, 1, 1),
+('Avengers: Endgame', false, 1, 1),
+('The Hangover', false, 1, 1),
+('Arrival', false, 1, 1),
+('Bohemian Rapsody', false, 1, 1);
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -9,19 +9,49 @@
 //   });;
 // });
 
+
+/**function to generate a category (html) */
+const createTweetElement = function (tweet) {
+  // header
+  let $category = $('<main>').addClass('category');
+  let $header = $('<header>').addClass('category-header');
+  let $categoryIcon = $('<i>').addClass('fas fa-caret-down');
+  $div.append($profilePic);
+  let $span = $('<span>').text(tweet.user.name).addClass('name');
+  $div.append($span);
+  $header.append($div);
+  let handle = $('<span>').text(tweet.user.handle).addClass('handle');
+  $header.append(handle);
+  $tweet.append($header);
+
+  //content
+  let $main = $('<main>').text(tweet.content.text);
+  $tweet.append($main);
+
+  //footer
+  let date = new Date(tweet.created_at);
+  let timeElapsed = timeSince(date);
+  let $footer = $('<footer>').addClass('container')
+  let $timeSince = $('<span>').text(`${timeElapsed} ago`);
+  $footer.append($timeSince);
+  let $icons = $('<span>')
+  let $i2 = $('<i>').addClass('fas fa-flag reaction');
+  $icons.append($i2);
+  let $i = $('<i>').addClass('fas fa-retweet reaction');
+  $icons.append($i);
+  let $i3 = $('<i>').addClass('fas fa-heart reaction');
+  $icons.append($i3);
+  $footer.append($icons);
+  $tweet.append($footer);
+  return $tweet;
+}
+
 $(document).ready(function () {
-
   const $categoryToggle = $(".category-header");
-
-    //Toggles the display of category icon
   $categoryToggle.on("click", function () {
+    //Toggles the display of category icon
     $(this).children("i").toggleClass("open")
-
     //Toggles the display of category items box
     $(this).siblings(".category-items").slideToggle(700);
-
   });
-
-
-
 });
