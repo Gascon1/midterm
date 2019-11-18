fb1119c8171a3f4861bf3b70d92eba961282c62a
SergeRodriguez <serge.rodriguez@mail.mcgill.ca>
2019-11-18 01:18:32 +0000

    add toggle functionality to Movies category

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,12 +1,22 @@
-$(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users.users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
-});
+// $(() => {
+//   $.ajax({
+//     method: "GET",
+//     url: "/api/users"
+//   }).done((users) => {
+//     for(user of users.users) {
+//       $("<div>").text(user.name).appendTo($("body"));
+//     }
+//   });;
+// });
 
-//test
+$(document).ready(function () {
+
+  //Toggles the display of category items box
+  const $categoryToggle = $(".header-toggle");
+  $categoryToggle.on("click", function () {
+    $(".category-items").slideToggle(700, function () {
+    });
+
+  });
+
+})
