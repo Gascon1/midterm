74317e0be68515927499b356db918dc23dd2be24
Mikel Gascon <mikelgascon@outlook.com>
2019-11-18 23:10:43 +0000

    textarea now in a form and submits to /new

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -37,4 +37,11 @@ $(document).ready(function () {
     }
   })
 
+  $('#add-item').keypress(function (e) {
+    if (e.keyCode == 13 && !e.shiftKey) {
+      e.preventDefault();
+      this.form.submit();
+    }
+  });
+
 });
