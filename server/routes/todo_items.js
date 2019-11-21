fc73d66f952cc847fcf8b428415a09114faa9ee7
Mikel Gascon <mikelgascon@outlook.com>
2019-11-21 01:57:29 +0000

    if a user searches with a specified keyword, it goes in the appropriate list

--- server/routes/todo_items.js
+++ server/routes/todo_items.js
@@ -28,38 +28,127 @@ module.exports = (db) => {
   });
 
   router.post("/", (req, res) => {
-    // console.log(req.body.text)
-    const encodedUserInput = encodeURI(req.body.text);
-    const AppId = "YJ5XA9-EUGVJUHHHH";
-    const URL = `http://api.wolframalpha.com/v2/query?input=${encodedUserInput}&output=json&appid=${AppId}`;
+    let userTodoItem = req.body.text
 
-
-    request(URL, (a, b, c) => findCategory(a, b, c, function (myTodoItems) {
-
-      console.log(c)
-      for (const item of myTodoItems) {
-
-        const query = {
-          text: `INSERT INTO todo_items (name, category_id, user_id)
+    if (userTodoItem.search('eat') >= 0) {
+      const query = {
+        text: `INSERT INTO todo_items (name, category_id, user_id)
           VALUES ($1, $2, $3)`,
-          values: [req.body.text, item.category, item.user_id]
-        };
+        values: [req.body.text, 2, 1]
+      };
 
-        db.query(query)
-          .then(data => {
-            // console.log(data.rows)
-            // console.log(data)
-            const todo_items = data.rows;
-            res.json({ todo_items });
-          })
-          .catch(err => {
-            res
-              .status(500)
-              .json({ error: err.message });
-          });
-      }
-    }))
+      db.query(query)
+        .then(data => {
+          // console.log(data.rows)
+          // console.log(data)
+          const todo_items = data.rows;
+          res.json({ todo_items });
+        })
+        .catch(err => {
+          console.log('we are in teh catch')
+          res
+            .status(500)
+            .json({ error: err.message });
+        });
+    }
+    if (userTodoItem.search('read') >= 0) {
+      const query = {
+        text: `INSERT INTO todo_items (name, category_id, user_id)
+          VALUES ($1, $2, $3)`,
+        values: [req.body.text, 3, 1]
+      };
 
+      db.query(query)
+        .then(data => {
+          // console.log(data.rows)
+          // console.log(data)
+          const todo_items = data.rows;
+          res.json({ todo_items });
+        })
+        .catch(err => {
+          console.log('we are in teh catch')
+          res
+            .status(500)
+            .json({ error: err.message });
+        });
+    }
+
+    if (userTodoItem.search('watch') >= 0) {
+      const query = {
+        text: `INSERT INTO todo_items (name, category_id, user_id)
+          VALUES ($1, $2, $3)`,
+        values: [req.body.text, 1, 1]
+      };
+
+      db.query(query)
+        .then(data => {
+          // console.log(data.rows)
+          // console.log(data)
+          const todo_items = data.rows;
+          res.json({ todo_items });
+        })
+        .catch(err => {
+          console.log('we are in teh catch')
+          res
+            .status(500)
+            .json({ error: err.message });
+        });
+    }
+
+    if (userTodoItem.search('buy') >= 0) {
+      const query = {
+        text: `INSERT INTO todo_items (name, category_id, user_id)
+          VALUES ($1, $2, $3)`,
+        values: [req.body.text, 4, 1]
+      };
+
+      db.query(query)
+        .then(data => {
+          // console.log(data.rows)
+          // console.log(data)
+          const todo_items = data.rows;
+          res.json({ todo_items });
+        })
+        .catch(err => {
+          console.log('we are in teh catch')
+          res
+            .status(500)
+            .json({ error: err.message });
+        });
+    }
+
+    else {
+
+      const encodedUserInput = encodeURI(req.body.text);
+      const AppId = "YJ5XA9-EUGVJUHHHH";
+      const URL = `http://api.wolframalpha.com/v2/query?input=${encodedUserInput}&output=json&appid=${AppId}`;
+
+
+      request(URL, (a, b, c) => findCategory(a, b, c, function (myTodoItems) {
+
+        for (const item of myTodoItems) {
+
+          const query = {
+            text: `INSERT INTO todo_items (name, category_id, user_id)
+          VALUES ($1, $2, $3)`,
+            values: [req.body.text, item.category, item.user_id]
+          };
+
+          db.query(query)
+            .then(data => {
+              // console.log(data.rows)
+              // console.log(data)
+              const todo_items = data.rows;
+              res.json({ todo_items });
+            })
+            .catch(err => {
+              res
+                .status(500)
+                .json({ error: err.message });
+            });
+        }
+      }))
+    }
     // console.log(request(URL, findCategory))
 
   });
